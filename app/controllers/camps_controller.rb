8ff2363b554f72b62245b92936700f20457845bd
Leo Ying <zying@andrew.cmu.edu>
2014-05-03 17:45:00 -0400

    Registration partly done

--- app/controllers/camps_controller.rb
+++ app/controllers/camps_controller.rb
@@ -10,6 +10,7 @@ class CampsController < ApplicationController
   def show
     @instructors = @camp.instructors.alphabetical.to_a
     @regs=@camp.registrations.by_student
+    @registration=Registration.new
   end
 
   def new
@@ -47,6 +48,6 @@ class CampsController < ApplicationController
     end
 
     def camp_params
-      params.require(:camp).permit(:curriculum_id, :location_id,:cost, :start_date, :end_date, :time_slot, :max_students, :active,:payment_status=>[], :instructor_ids => [],:student_ids=>[])
+      params.require(:camp).permit(:curriculum_id, :location_id,:cost, :start_date, :end_date, :time_slot, :max_students, :active, :instructor_ids => [],:student_ids=>[])
     end
 end
---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,70 @@
+class RegistrationsController < ApplicationController
+  before_action :set_registration, only: [:show, :edit, :update, :destroy]
+
+  # GET /registrations
+  # GET /registrations.json
+  def index
+    @registrations = Registration.all
+  end
+
+  # GET /registrations/1
+  # GET /registrations/1.json
+  def show
+  end
+
+  # GET /registrations/new
+  def new
+    @registration = Registration.new
+  end
+
+  # GET /registrations/1/edit
+  def edit
+  end
+
+  # POST /registrations
+  # POST /registrations.json
+  def create
+    @registration = Registration.new(registration_params)
+      if @registration.save
+        redirect_to @registration.camp, notice:"The registration was created successfully"
+      else
+        render 'registrations/new'
+      end
+    
+  end
+
+  # PATCH/PUT /registrations/1
+  # PATCH/PUT /registrations/1.json
+  def update
+    respond_to do |format|
+      if @registration.update(registration_params)
+        format.html { redirect_to @registration, notice: 'Camp instructor was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @registration.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /registrations/1
+  # DELETE /registrations/1.json
+  def destroy
+    @registration.destroy
+    respond_to do |format|
+      format.html { redirect_to registrations_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_registration
+      @registration = Registration.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def registration_params
+      params.require(:registration).permit(:id,:temp_id, :camp_id, :student_id,:payment_status,:points_earned)
+    end
+end
